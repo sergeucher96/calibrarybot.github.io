a5d51edd6f3937e370c6e58450c31a5d2e4a71ab
Sergeu <sergeu.cher96@gmail.com>
2024-09-04 14:46:57 -0600

    kategorii

---
+++ script.js
@@ -0,0 +1,21 @@
+// Получаем все элементы с классом "category-block"
+const categoryBlocks = document.querySelectorAll('.category-block');
+// Получаем все иконки приложений
+const appIcons = document.querySelectorAll('.app-icon');
+
+categoryBlocks.forEach(block => {
+    block.addEventListener('click', (e) => {
+        e.preventDefault(); // Предотвращаем переход по ссылке
+        const category = block.dataset.category; // Получаем выбранную категорию
+
+        // Перебираем все иконки
+        appIcons.forEach(icon => {
+            const categories = icon.dataset.categories.split(', '); // Получаем массив категорий иконки
+            if (category === 'all' || categories.includes(category)) {
+                icon.parentElement.style.display = 'block'; // Показываем иконку
+            } else {
+                icon.parentElement.style.display = 'none'; // Скрываем иконку
+            }
+        });
+    });
+});
